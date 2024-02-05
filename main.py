87a21b0ea34bc1e4bea88bc01e8f5cc48428cd73
Mily-the-coder <147286542+Mily-the-coder@users.noreply.github.com>
2024-02-05 13:01:53 -0300

    Changed console from many elif statements to a match statement. (just found out it exists in python and honestly im in love)

--- main.py
+++ main.py
@@ -31,49 +31,49 @@ while In_Console:
         cmd = last
         get_last = False
 
-    if cmd[0] == 'set':
+    match cmd[0]:
+        case  'set':
+            cmd[1], cmd[2] = int(cmd[1]), int(cmd[2])
 
-        cmd[1], cmd[2] = int(cmd[1]), int(cmd[2])
-
-        if int(cmd[1]) > width or int(cmd[2]) > height:
-            print('Failed to set cell: Out of range')
-        else:
-            if cellGrid.getCell((cmd[1], cmd[2])) == 0:
-                value = 1
+            if int(cmd[1]) > width or int(cmd[2]) > height:
+                print('Failed to set cell: Out of range')
             else:
-                value = 0
-            cellGrid.setCell((cmd[1], cmd[2]), value)
+                if cellGrid.getCell((cmd[1], cmd[2])) == 0:
+                    value = 1
+                else:
+                    value = 0
+                cellGrid.setCell((cmd[1], cmd[2]), value)
 
+                show_cells()
+            last = copy.copy(cmd)
+
+        case 'show':
             show_cells()
-        last = copy.copy(cmd)
 
-    elif cmd[0] == 'show':
-        show_cells()
+        case 'run':
+            if len(cmd) == 1:
+                timer = 0.5
+            else:
+                timer = float(cmd[1])
+            In_Console = False
 
-    elif cmd[0] == 'run':
-        if len(cmd) == 1:
-            timer = 0.5
-        else:
-            timer = float(cmd[1])
-        In_Console = False
+        case 'last':
+            get_last = True
 
-    elif cmd[0] == 'last':
-        get_last = True
-
-    elif cmd[0] == 'help':
-        print("""-==|Commands|==-\n
-    set  -Sets cell in grid to opposite of its value.\n
-         -Syntax: > set [Tile X] [Tile Y]  ->  set 16 8\n
-         \n
-    show -Displays entire grid.\n
-         -Syntax: > show\n
-         \n
-    last -Uses last command.\n
-         -Syntax: > last\n
-         \n
-    run  -Runs program with optional time between generations (in seconds).\n
-         -Syntax: > run [Time]  -> run 1.5   (if parameter time is empty default time is 0.5 seconds)\n
-         """)
+        case 'help':
+            print("""-==|Commands|==-\n
+        set  -Sets cell in grid to opposite of its value.\n
+             -Syntax: > set [Tile X] [Tile Y]  ->  set 16 8\n
+             \n
+        show -Displays entire grid.\n
+             -Syntax: > show\n
+             \n
+        last -Uses last command.\n
+             -Syntax: > last\n
+             \n
+        run  -Runs program with optional time between generations (in seconds).\n
+             -Syntax: > run [Time]  -> run 1.5   (if parameter time is empty default time is 0.5 seconds)\n
+             """)
 
 # main game loop
 while not In_Console:
